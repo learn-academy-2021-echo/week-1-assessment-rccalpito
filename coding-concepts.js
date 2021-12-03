ed199d2bbaee54e9ff44b79ff7ee5382140f0a55
rccalpito <rccalpito@gmail.com>
2021-12-03 15:57:39 -0800

    Completed code-challenges.js and coding-concepts.js. Halfway complete with interview-questions.md

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,29 +8,29 @@
 // --------------------1) What will this log?
 
 var cohort = "Echo 2021"
-// console.log(cohort.length)
+console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 9
+// b) Verify and explain: I received the expected output. The console is told to output the length of the variable cohort
 
 
 // --------------------2) What will this log?
 
 var greeting = "Hello World!"
-// console.log(greeting[3])
+console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "l" 
+// b) Verify and explain: I received the expected output. The code is asked to return to console the 3rd index of "hello world!"
 
 
 // --------------------3) What will this log?
 
 var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
-// console.log(languages[index])
+console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Ruby"
+// b) Verify and explain:  the code is asking to output the 1st index of the variable array language.
 
 
 // --------------------4) What will this log?
@@ -38,14 +38,14 @@ var index = 1
 var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ["SATURDAY", "SUNDAY"]
+// b) Verify and explain: My answer was incorrect. The .toUpperCase() method only works on strings. To upper case each individual index, you would have to reference the specific index in the array e.g weekendDays[1].toUpperCase()
 
 
 // --------------------5) What will this log?
 
 var dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: number
+// b) Verify and explain: the function typeof returns the type of primitive datatype of the length of the array datatypes
